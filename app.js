8e29f705d06672becf4c6c42eec4d1b981740bb4
JonathanRVega <Jonathaan.rv@gmail.com>
2020-11-11 17:43:50 -0700

    Added project

---
+++ app.js
@@ -0,0 +1,474 @@
+//1
+console.log("Exercise 1")
+
+
+const prepareTea = () => 'greenTea';
+
+
+const getTea = (numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+const tea4TeamFCC = getTea(40);
+
+console.log(tea4TeamFCC)
+
+
+//2
+console.log("Exercise 2")
+
+
+const prepareGreenTea = () => 'greenTea';
+
+const prepareBlackTea = () => 'blackTea';
+
+const getTea2 = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+const tea4GreenTeamFCC = getTea2(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea2(prepareBlackTea, 13);
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+
+//3
+console.log("Exercise 3")
+
+// tabs is an array of titles of each site open within the window
+var Window = function(tabs) {
+    this.tabs = tabs; // We keep a record of the array inside the object
+  };
+  
+  // When you join two windows into one window
+  Window.prototype.join = function (otherWindow) {
+    this.tabs = this.tabs.concat(otherWindow.tabs);
+    return this;
+  };
+  
+  // When you open a new tab at the end
+  Window.prototype.tabOpen = function (tab) {
+    this.tabs.push('new tab'); // Let's open a new tab for now
+    return this;
+  };
+  
+  // When you close a tab
+  Window.prototype.tabClose = function (index) {
+  
+    // Only change code below this line
+  
+    var tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
+    var tabsAfterIndex = this.tabs.splice(1); // Get the tabs after the tab
+  
+    this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
+  
+    // Only change code above this line
+  
+    return this;
+   };
+  
+  // Let's create three browser windows
+  var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+  var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+  var videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+  
+  // Now perform the tab opening, closing, and other operations
+  var finalTabs = socialWindow
+    .tabOpen() // Open a new tab for cat memes
+    .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+    .join(workWindow.tabClose(1).tabOpen());
+  console.log(finalTabs.tabs);
+
+  
+  //4
+  console.log("Exercise 4")
+
+  
+var fixedValue = 4;
+
+function incrementer () {
+  return fixedValue + 1
+}
+
+console.log(incrementer(fixedValue))
+
+//5
+console.log("Exercise 5")
+
+var fixedValue = 4;
+
+function incrementer (value) {
+value = value + 1
+return value
+
+}
+
+console.log(incrementer(fixedValue))
+
+//6
+console.log("Exercise 6")
+
+
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+function add (arr, bookName) {
+
+  let newArray = [...arr];
+  newArray.push(bookName);
+  return newArray;
+  
+}
+
+function remove (arr, bookName) {
+  let newArray = [...arr];
+  if (newArray.indexOf(bookName) >= 0) {
+    newArray.splice(newArray.indexOf(bookName), 1);
+    return newArray;
+
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
+
+
+//7
+console.log("Exercise 7")
+
+var watchList = [
+    {
+      "Title": "Inception",
+      "Year": "2010",
+      "Rated": "PG-13",
+      "Released": "16 Jul 2010",
+      "Runtime": "148 min",
+      "Genre": "Action, Adventure, Crime",
+      "Director": "Christopher Nolan",
+      "Writer": "Christopher Nolan",
+      "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+      "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+      "Language": "English, Japanese, French",
+      "Country": "USA, UK",
+      "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+      "Metascore": "74",
+      "imdbRating": "8.8",
+      "imdbVotes": "1,446,708",
+      "imdbID": "tt1375666",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "Interstellar",
+      "Year": "2014",
+      "Rated": "PG-13",
+      "Released": "07 Nov 2014",
+      "Runtime": "169 min",
+      "Genre": "Adventure, Drama, Sci-Fi",
+      "Director": "Christopher Nolan",
+      "Writer": "Jonathan Nolan, Christopher Nolan",
+      "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+      "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      "Language": "English",
+      "Country": "USA, UK",
+      "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+      "Metascore": "74",
+      "imdbRating": "8.6",
+      "imdbVotes": "910,366",
+      "imdbID": "tt0816692",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "The Dark Knight",
+      "Year": "2008",
+      "Rated": "PG-13",
+      "Released": "18 Jul 2008",
+      "Runtime": "152 min",
+      "Genre": "Action, Adventure, Crime",
+      "Director": "Christopher Nolan",
+      "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+      "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+      "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+      "Language": "English, Mandarin",
+      "Country": "USA, UK",
+      "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+      "Metascore": "82",
+      "imdbRating": "9.0",
+      "imdbVotes": "1,652,832",
+      "imdbID": "tt0468569",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "Batman Begins",
+      "Year": "2005",
+      "Rated": "PG-13",
+      "Released": "15 Jun 2005",
+      "Runtime": "140 min",
+      "Genre": "Action, Adventure",
+      "Director": "Christopher Nolan",
+      "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+      "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+      "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+      "Language": "English, Urdu, Mandarin",
+      "Country": "USA, UK",
+      "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+      "Metascore": "70",
+      "imdbRating": "8.3",
+      "imdbVotes": "972,584",
+      "imdbID": "tt0372784",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "Avatar",
+      "Year": "2009",
+      "Rated": "PG-13",
+      "Released": "18 Dec 2009",
+      "Runtime": "162 min",
+      "Genre": "Action, Adventure, Fantasy",
+      "Director": "James Cameron",
+      "Writer": "James Cameron",
+      "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+      "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+      "Language": "English, Spanish",
+      "Country": "USA, UK",
+      "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+      "Metascore": "83",
+      "imdbRating": "7.9",
+      "imdbVotes": "876,575",
+      "imdbID": "tt0499549",
+      "Type": "movie",
+      "Response": "True"
+    }
+  ];
+  
+  
+  const ratings = watchList.map(item => ({
+    title: item["Title"],
+    rating: item["imdbRating"]
+  }));
+  
+  
+  console.log(JSON.stringify(ratings));
+
+  
+  //8
+  console.log("Exercise 8")
+
+  var s = [23, 65, 98, 5];
+Array.prototype.myMap = function(callback) {
+    var newArray = [];
+     for (let i = 0; i < this.length; i++) {
+      newArray.push(callback(this[i]));
+    }
+    return newArray;
+  };
+  
+  var new_s = s.myMap(function(item) {
+    return item * 2;
+  });
+
+  console.log(new_s)
+
+  //9
+  console.log("Exercise 9")
+
+  var watchList = [
+    {
+      "Title": "Inception",
+      "Year": "2010",
+      "Rated": "PG-13",
+      "Released": "16 Jul 2010",
+      "Runtime": "148 min",
+      "Genre": "Action, Adventure, Crime",
+      "Director": "Christopher Nolan",
+      "Writer": "Christopher Nolan",
+      "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+      "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+      "Language": "English, Japanese, French",
+      "Country": "USA, UK",
+      "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+      "Metascore": "74",
+      "imdbRating": "8.8",
+      "imdbVotes": "1,446,708",
+      "imdbID": "tt1375666",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "Interstellar",
+      "Year": "2014",
+      "Rated": "PG-13",
+      "Released": "07 Nov 2014",
+      "Runtime": "169 min",
+      "Genre": "Adventure, Drama, Sci-Fi",
+      "Director": "Christopher Nolan",
+      "Writer": "Jonathan Nolan, Christopher Nolan",
+      "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+      "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      "Language": "English",
+      "Country": "USA, UK",
+      "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+      "Metascore": "74",
+      "imdbRating": "8.6",
+      "imdbVotes": "910,366",
+      "imdbID": "tt0816692",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "The Dark Knight",
+      "Year": "2008",
+      "Rated": "PG-13",
+      "Released": "18 Jul 2008",
+      "Runtime": "152 min",
+      "Genre": "Action, Adventure, Crime",
+      "Director": "Christopher Nolan",
+      "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+      "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+      "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+      "Language": "English, Mandarin",
+      "Country": "USA, UK",
+      "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+      "Metascore": "82",
+      "imdbRating": "9.0",
+      "imdbVotes": "1,652,832",
+      "imdbID": "tt0468569",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "Batman Begins",
+      "Year": "2005",
+      "Rated": "PG-13",
+      "Released": "15 Jun 2005",
+      "Runtime": "140 min",
+      "Genre": "Action, Adventure",
+      "Director": "Christopher Nolan",
+      "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+      "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+      "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+      "Language": "English, Urdu, Mandarin",
+      "Country": "USA, UK",
+      "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+      "Metascore": "70",
+      "imdbRating": "8.3",
+      "imdbVotes": "972,584",
+      "imdbID": "tt0372784",
+      "Type": "movie",
+      "Response": "True"
+    },
+    {
+      "Title": "Avatar",
+      "Year": "2009",
+      "Rated": "PG-13",
+      "Released": "18 Dec 2009",
+      "Runtime": "162 min",
+      "Genre": "Action, Adventure, Fantasy",
+      "Director": "James Cameron",
+      "Writer": "James Cameron",
+      "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+      "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+      "Language": "English, Spanish",
+      "Country": "USA, UK",
+      "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+      "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+      "Metascore": "83",
+      "imdbRating": "7.9",
+      "imdbVotes": "876,575",
+      "imdbID": "tt0499549",
+      "Type": "movie",
+      "Response": "True"
+    }
+  ];
+  
+  var filteredList = watchList
+    .map(movie => {
+      return {
+        title: movie.Title,
+        rating: movie.imdbRating
+      };
+    })
+    .filter(movie => {
+      return parseFloat(movie.rating) >= 8.0;
+    });
+  
+  
+  console.log(filteredList);
+
+  //10
+
+  console.log("Exercise 10")
+
+  
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+   var newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i]) === true) {
+      newArray.push(this[i]);
+    }
+  }
+  return newArray;
+};
+
+var new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
+
+console.log(new_s)
+
+
+//11
+console.log("Exercise 11")
+
+function sliceArray(anim, beginSlice, endSlice) {
+  
+    return anim.slice(beginSlice, endSlice)
+    
+    }
+    var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+    sliceArray(inputAnim, 1, 3);
+    
+console.log(sliceArray(inputAnim, 1, 3))
+
+//12
+console.log("Exercise 12")
+
+function nonMutatingSplice(cities) {
+  
+    return cities.slice(0,3);
+  
+  }
+  var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+  nonMutatingSplice(inputCities);
+
+  console.log(nonMutatingSplice(inputCities))
